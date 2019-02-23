04c1b22025b9d226ee4ea11a732f26b7f0ff9a3b
김기연 <kyean07@naver.com>
2019-02-24 03:13:57 +0900

    Test enrolment xml +save button added ..etc

--- app/src/main/java/com/teammgh/cnboard/Global.java
+++ app/src/main/java/com/teammgh/cnboard/Global.java
@@ -1,9 +1,14 @@
 package com.teammgh.cnboard;
 
+import java.util.ArrayList;
+
 public class Global {
 
     public static int subjectIndex = 0;
     public static int categoryNo = 0;
     public static int grade = 0;
+
+    public static ArrayList<String> mySubject = new ArrayList<String>();
+
 
 }
