5e18a82085923263ec406b87629644f371eeb238
Kim <lmkn5342@gmail.com>
2019-02-23 23:50:58 +0900

    디데이자바

---
+++ app/src/main/java/com/teammgh/cnboard/DdayActivity.java
@@ -0,0 +1,153 @@
+package com.teammgh.cnboard;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class DdayActivity extends AppCompatActivity {
+
+    EditText edittext_title;
+    LinearLayout linear_date;
+    Switch switch_alert;
+    Button button_cancel;
+    Button button_ok;
+    TextView textview_mday;
+    boolean checking = false;
+    final int DIALOG_DATE = 1;
+    private int mYear, mMonth, mDay;
+    private int dYear = 1 , dMonth = 1 , dDay = 1;
+    private long d;
+    private long t;
+    private long r;
+    private int result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dday);
+
+        edittext_title = findViewById(R.id.edittext_title);
+        linear_date = findViewById(R.id.linear_date);
+        switch_alert = findViewById(R.id.switch_alert);
+        button_cancel = findViewById(R.id.button_cancel);
+        button_ok = findViewById(R.id.button_ok);
+        textview_mday = findViewById(R.id.textview_mday);
+
+        Calendar cal = Calendar.getInstance();
+        mYear = cal.get(Calendar.YEAR);
+        mMonth = cal.get(Calendar.MONTH) + 1;
+        mDay = cal.get(Calendar.DAY_OF_MONTH);
+
+        textview_mday.setText(mYear + "년 " +mMonth + "월 " + mDay + "일 ");
+
+        Calendar dcal = Calendar.getInstance();
+        dcal.set(dYear,dMonth,dDay);
+
+        t = cal.getTimeInMillis();
+        d = dcal.getTimeInMillis();
+        r = (d-t)/(24*60*60*1000);
+        result = (int)r + 1 ;
+
+        linear_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog dialog =  new DatePickerDialog(DdayActivity.this, dDateSstListener, mYear, mMonth - 1, mDay);
+                dialog.show();
+            }
+        });
+
+        switch_alert.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked == true) {
+                    Toast.makeText(getApplicationContext(), "알림이 설정되었습니다.",Toast.LENGTH_SHORT).show();
+                    checking = true;
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "알림이 해제되었습니다.",Toast.LENGTH_SHORT).show();
+                    checking = false;
+                }
+            }
+        });
+
+        button_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"취소되었습니다." ,Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(),MainActivity_Dday.class);
+                setResult(RESULT_CANCELED);
+                finish();
+            }
+        });
+
+        button_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = edittext_title.getText().toString();
+
+                if(title.isEmpty() == true) {
+                    Toast.makeText(getApplicationContext(),"제목을 설정해주세요." ,Toast.LENGTH_SHORT).show();
+                }
+
+                else{
+                    if(dYear == 1 && dMonth == 1 && dDay == 1) {
+                        Toast.makeText(getApplicationContext(),"날짜를 설정해주세요", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "D-DAY 가 등록되었습니다.", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent();
+                        intent.putExtra("Dday", result);
+                        intent.putExtra("Title", title);
+                        intent.putExtra("year", dYear);
+                        intent.putExtra("month", dMonth);
+                        intent.putExtra("day", dDay);
+                        intent.putExtra("check",checking);
+                        setResult(RESULT_OK, intent);
+
+                        finish();
+                    }
+                }
+            }
+        });
+    }
+    private DatePickerDialog.OnDateSetListener dDateSstListener = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+            dYear = i;
+            dMonth = i1;
+            dDay = i2;
+
+            final Calendar dcal = Calendar.getInstance();
+            dcal.set(dYear,dMonth,dDay);
+
+            d = dcal.getTimeInMillis();
+            r = (d-t)/(24*60*60*1000);
+            result = (int)r;
+            dMonth = dMonth + 1;
+
+            textview_mday.setText(dYear + "년 " + dMonth + "월 " + dDay + "일 ");
+        }
+    };
+    protected Dialog onCreateDialog (int id) {
+        switch (id) {
+            case DIALOG_DATE :
+                return new DatePickerDialog(this,dDateSstListener,mYear,mMonth,mDay);
+            default:
+                return null;
+        }
+    }
+}
