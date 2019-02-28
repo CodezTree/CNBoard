1ff6d986d9fd0d2d0039010aeee5d727f17c26e2
Kim <lmkn5342@gmail.com>
2019-02-28 22:13:50 +0900

    약간 수정

--- app/src/main/java/com/teammgh/cnboard/DdayActivity.java
+++ app/src/main/java/com/teammgh/cnboard/DdayActivity.java
@@ -30,7 +30,7 @@ public class DdayActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_ddaymain);
 
         button = (Button) findViewById(R.id.button_add_dday);
         listView = (ListView) findViewById(R.id.listview);
