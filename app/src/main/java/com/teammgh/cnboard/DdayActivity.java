5a698e705963035aa06acfb552cd3cceac3c00ad
LOLHIA <test1@naver.com>
2019-03-05 22:28:42 +0900

    COMMMMIT

--- app/src/main/java/com/teammgh/cnboard/DdayActivity.java
+++ app/src/main/java/com/teammgh/cnboard/DdayActivity.java
@@ -38,7 +38,7 @@ public class DdayActivity extends AppCompatActivity {
     long mday;
     Toolbar myToolbar;
 
-    String test3;
+    String test4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
